8f504750bce17ec7737107cbd7412bdd57c1e50e
Sharmina Akhtar <sakht318@gmail.com>
2018-09-16 14:36:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,65 +1,58 @@
+const baseURL = 'https://api.github.com';
+const user = '<YOUR_USERNAME>';
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-
   return '';
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-
-  fetch('https://github.com/repos/learn-co-curriculum/js-ajax-fetch-lab/forks',{
+  const url = `${baseURL}/repos/${repo}/forks`;
+  fetch(url, {
     method: 'POST',
     headers: {
-    Authorization: `token ${getToken()}`
+      Authorization: `token ${getToken()}`
     }
   })
-    .then(res => res.json());
+    .then(res => res.json())
     .then(json => showResults(json));
-      })
-  //use fetch to fork it!
 }
 
 function showResults(json) {
   document.getElementById('results').innerHTML = `<a href=${json.html_url}>${
-    json.html_url}</a>`;
-  //use this function to display the results from forking via the API
+    json.html_url
+  }</a>`;
 }
 
 function createIssue() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  const repo = `${user}/js-ajax-fetch-lab`;
   const url = `${baseURL}/repos/${repo}/issues`;
-
   const postData = {
     title: document.getElementById('title').value,
-     body: document.getElementById('body').value
-   };
+    body: document.getElementById('body').value
+  };
 
-fetch('url',
-  {
+  fetch(url, {
     method: 'POST',
     body: JSON.stringify(postData),
     headers: {
       Authorization: `token ${getToken()}`
     }
   })
-  .then(res => res.json());
-  .then(json => json.getIssues());
-  //use this function to create an issue based on the values input in index.html
+    .then(res => res.json())
+    .then(json => getIssues());
 }
 
 function getIssues() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  const repo = `${user}/js-ajax-fetch-lab`;
   const url = `${baseURL}/repos/${repo}/issues`;
-
-  fetch('url',
-    {
-      headers: {
-        Authorization: `token ${getToken()}`
-      }
-    })
+  fetch(url, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
     .then(res => res.json())
-    .then (json => console.log(json));
-
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+    .then(json => console.log(json));
 }
